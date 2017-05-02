001275fb0e5dfe67ec6a04b09d728a14440dfcc6
Nick Rafter <nicholas.rafter@gmail.com>
2017-05-02 14:09:08 -0400

    forgot to add to vcs

---
+++ src/reducers/index.js
@@ -0,0 +1,48 @@
+import { combineReducers } from 'redux';
+import { NavigationActions } from 'react-navigation';
+import notes from './notes';
+import currentNote from './current';
+
+import { AppNavigator } from '../navigators/AppNavigator';
+
+const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Main'));
+
+function nav(state = initialState, action) {
+  let nextState;
+  switch (action.type) {
+    case 'Login':
+      nextState = AppNavigator.router.getStateForAction(NavigationActions.back(), state);
+      break;
+    case 'Logout':
+      nextState = AppNavigator.router.getStateForAction(NavigationActions.navigate({ routeName: 'Login' }), state);
+      break;
+    default:
+      nextState = AppNavigator.router.getStateForAction(action, state);
+      break;
+  }
+
+  // Simply return the original `state` if `nextState` is null or undefined.
+  return nextState || state;
+}
+
+const initialAuthState = { isLoggedIn: false };
+
+function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case 'Login':
+      return { ...state, isLoggedIn: true };
+    case 'Logout':
+      return { ...state, isLoggedIn: false };
+    default:
+      return state;
+  }
+}
+
+const AppReducer = combineReducers({
+  nav,
+  auth,
+  notes,
+  currentNote,
+});
+
+export default AppReducer;
